a1e4918c6e99d968bba0aa6d0ff92aee607703d5
onlyrsh <chankeilambda@gmail.com>
2024-03-11 21:19:25 +0500

    add: resources

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,23 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductController extends Controller
 {
 
-    public function index()
+    public function index() : AnonymousResourceCollection
     {
-        return Product::with('stocks')->get();
+        return ProductResource::collection(Product::cursorPaginate(25));
     }
 
 
 
     public function store(StoreProductRequest $request)
     {
-        //
+
     }
 
 
@@ -30,12 +32,12 @@ class ProductController extends Controller
 
     public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+
     }
 
 
     public function destroy(Product $product)
     {
-        //
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
-
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CategoryProductController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,4 +14,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResources([
     'categories' => CategoryController::class,
     'products' => ProductController::class,
+    'categories.products' => CategoryProductController::class,
 ]);
+
+
